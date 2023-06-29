e462d48dc2fa674d70c7f5154dc9ed08e7aac28f
bjexe <bensimms@knights.ucf.edu>
2023-06-29 12:25:37 -0400

    +page layout, rename Counter to Item and implement basic functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,32 @@
-<h1 class="text-4xl font-bold underline">
-    Hello world!
+<script>
+    import Item from './Item.svelte';
+    let checklistItems = [
+        {
+            description: "Leetcode problems",
+            goal: 4
+        },
+        {
+            description: "Job apps",
+            goal: 10
+        },
+        {
+            description: "Learn something new",
+            goal: 1
+        },
+        {
+            description: "Exercise",
+            goal: 1
+        }
+    ];
+</script>
+
+<h1 class="text-4xl font-bold underline text-center mt-[2%]">
+    Checklist
 </h1>
+
+{#each checklistItems as item (item)}
+    <Item description={item.description} goal={item.goal}/>
+{/each}
 
 <style lang="postcss">
     :global(html) {
---
+++ src/routes/Item.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let goal;
+    export let description;
+    let count = 0;
+    function increase() {
+        count = count + 1;
+    }
+    function decrease(){
+        count = count > 0 ? count - 1 : 0;
+    }
+</script>
+
+<div class="border flex flex-col items-center">
+    <h2 class="text-2xl">{description}</h2>
+    <h3 class="text-2xl">{count}/{goal}</h3>
+    <span>
+        <button class="text-3xl" on:click={increase}>+</button>
+        <button class="text-3xl" on:click={decrease}>-</button>
+    </span>
+</div>
