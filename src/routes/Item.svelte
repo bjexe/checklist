165a180e7e1369d6fdef1af6e69f1d9ba6d205db
bjexe <bensimms@knights.ucf.edu>
2023-06-29 18:57:12 -0400

    update Item.svelte

--- src/routes/Item.svelte
+++ src/routes/Item.svelte
@@ -10,11 +10,18 @@
     }
 </script>
 
-<div class="border flex flex-col items-center">
-    <h2 class="text-2xl">{description}</h2>
-    <h3 class="text-2xl">{count}/{goal}</h3>
-    <span>
-        <button class="text-3xl" on:click={increase}>+</button>
-        <button class="text-3xl" on:click={decrease}>-</button>
-    </span>
+<div class="flex flex-col justify-center items-center mt-[2%]">
+    <div class="flex flex-col items-center rounded-3xl lg:w-[20%] bg-[#F1F2EE] gap-y-[15px]">
+        <h2 class="text-2xl mt-[5px]">{description}</h2>
+        <span class="flex gap-x-10 justify-center items-center">
+            <h3 class="text-2xl">{count}/{goal}</h3>
+            {#if count === goal}
+                <p class="text-2xl">✔️</p>
+            {/if}
+        </span>
+        <span>
+            <button class="text-3xl" on:click={increase}>+</button>
+            <button class="text-3xl" on:click={decrease}>-</button>
+        </span>
+    </div>
 </div>
